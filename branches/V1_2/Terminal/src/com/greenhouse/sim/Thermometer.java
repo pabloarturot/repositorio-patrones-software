235c84720119eedd9e21fce4b871c969c355e25c
varonmarcos@gmail.com <varonmarcos@gmail.com@879684ba-57c7-7e35-b14b-5a3cab2a764a>
2013-02-17 19:07:34 +0000

    version inicial de Terminal

---
+++ branches/V1_2/Terminal/src/com/greenhouse/sim/Thermometer.java
@@ -0,0 +1,12 @@
+package com.greenhouse.sim;
+
+import java.util.Random;
+
+public class Thermometer {
+
+	public static double getTemperature() {
+        Random randomGenerator = new Random();
+        return (randomGenerator.nextDouble()+10)*randomGenerator.nextInt(10);
+    }
+
+}
